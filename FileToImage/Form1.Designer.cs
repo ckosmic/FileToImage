1aa3993489190c78ba6e79ea2edde36c7b52c4e9
Christian Kosman <christiankosman@gmail.com>
2018-11-28 09:33:59 -0800

    Initial commits

---
+++ FileToImage/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace FileToImage {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(249, 81);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(255, 255);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(23, 298);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(122, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Choose file...";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.comboBox1);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.numericUpDown1);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(38, 81);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(171, 380);
+			this.panel1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(38, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(144, 26);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "File To Image";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(20, 65);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Image samples:";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(23, 82);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 4;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(20, 115);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(80, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Blending mode:";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Multiply",
+            "Overlay",
+            "Screen",
+            "Linear Dodge"});
+			this.comboBox1.Location = new System.Drawing.Point(23, 131);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(20, 20);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(59, 17);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Settings";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(23, 328);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(122, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Generate image";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(249, 370);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(255, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Save image...";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoEllipsis = true;
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(249, 432);
+			this.label5.MaximumSize = new System.Drawing.Size(250, 25);
+			this.label5.MinimumSize = new System.Drawing.Size(250, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(250, 13);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "No file selected";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(250, 400);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(252, 23);
+			this.progressBar1.Step = 1;
+			this.progressBar1.TabIndex = 6;
+			this.progressBar1.Value = 100;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(545, 503);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "File To Image";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
---
+++ FileToImage/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace FileToImage {
+	public partial class Form1 : Form {
+
+		public static int imageSamples = 3;
+
+		public string selectedFile;
+
+		public enum BlendingMode {
+			Multiply,
+			Overlay,
+			Screen,
+			LinearDodge
+		};
+
+		public Form1() {
+			InitializeComponent();
+			progressBar1.Visible = false;
+			comboBox1.SelectedIndex = 1;
+		}
+
+		public void CreateImage(string fileName) {
+			imageSamples = (int)numericUpDown1.Value;
+
+			imageSamples *= 2;
+
+			byte[] fileBytes = File.ReadAllBytes(fileName);
+			byte[] sampleBytes = new byte[imageSamples];
+
+			for (int i = 0; i < imageSamples; i++) {
+				sampleBytes[i] = fileBytes[fileBytes.Length / imageSamples * (i + 1) - 1];
+				if (sampleBytes[i] == 0) sampleBytes[i] = 255;
+			}
+
+			imageSamples /= 2;
+
+			int imgWidth = 255, imgHeight = 255;
+
+			Bitmap[] sampleImages = new Bitmap[imageSamples];
+			progressBar1.Visible = true;
+			for (int i = 0; i < sampleBytes.Length / 2; i++) {
+				progressBar1.Value = 100 * (i+1) / imageSamples;
+				using (WebClient wc = new WebClient()) {
+					int width = sampleBytes[i * 2];
+					//if (width % 2 == 1) width += 1;
+					int height = sampleBytes[i * 2 + 1];
+					//if (height % 2 == 1) height += 1;
+					Console.WriteLine(width + "x" + height);
+					byte[] imgBytes = wc.DownloadData("https://picsum.photos/" + width + "/" + height);
+
+
+					using (MemoryStream ms = new MemoryStream(imgBytes, 0, imgBytes.Length)) {
+						Image img = Image.FromStream(ms);
+						Bitmap bidmab = new Bitmap(img);
+						Bitmap bm = new Bitmap(imgWidth, imgHeight, PixelFormat.Format24bppRgb);
+						using (Graphics g = Graphics.FromImage(bm)) {
+							g.DrawImage(bidmab, new Rectangle(0, 0, imgWidth, imgHeight));
+						}
+						bidmab.Dispose();
+						sampleImages[i] = bm;
+					}
+				}
+			}
+
+			BlendingMode[] modes = { BlendingMode.Multiply, BlendingMode.Overlay, BlendingMode.Screen, BlendingMode.LinearDodge };
+			BlendingMode mode = modes[comboBox1.SelectedIndex];
+
+			Bitmap bmp = Blend(sampleImages[0], sampleImages[1], mode);
+			for (int i = 1; i < imageSamples; i++) {
+				bmp = Blend(bmp, sampleImages[i], mode);
+			}
+
+			pictureBox1.Image = bmp;
+
+			progressBar1.Visible = false;
+		}
+
+		public static Bitmap Blend(Bitmap src, Bitmap bln, BlendingMode blendingMode) {
+			BitmapData srcData = src.LockBits(new Rectangle(0, 0, src.Width, src.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+			byte[] pixelBuffer = new byte[srcData.Stride * srcData.Height];
+			Marshal.Copy(srcData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+			src.UnlockBits(srcData);
+
+			BitmapData blnData = bln.LockBits(new Rectangle(0, 0, bln.Width, bln.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+			byte[] blendBuffer = new byte[blnData.Stride * blnData.Height];
+			Marshal.Copy(blnData.Scan0, blendBuffer, 0, blendBuffer.Length);
+			bln.UnlockBits(blnData);
+
+			int r = 0, g = 0, b = 0;
+
+			for (int k = 0; (k + 4 < pixelBuffer.Length) && (k + 4 < blendBuffer.Length); k += 4) {
+				r = BlendingOperation(pixelBuffer[k + 2], blendBuffer[k + 2], blendingMode);
+				g = BlendingOperation(pixelBuffer[k + 1], blendBuffer[k + 1], blendingMode);
+				b = BlendingOperation(pixelBuffer[k + 0], blendBuffer[k + 0], blendingMode);
+
+				if (r < 0) r = 0; else if (r > 255) r = 255;
+				if (g < 0) g = 0; else if (g > 255) g = 255;
+				if (b < 0) b = 0; else if (b > 255) b = 255;
+
+				pixelBuffer[k + 2] = (byte)r;
+				pixelBuffer[k + 1] = (byte)g;
+				pixelBuffer[k + 0] = (byte)b;
+			}
+
+			Bitmap outputBmp = new Bitmap(src.Width, src.Height);
+
+			BitmapData outputData = outputBmp.LockBits(new Rectangle(0, 0, outputBmp.Width, outputBmp.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+			Marshal.Copy(pixelBuffer, 0, outputData.Scan0, pixelBuffer.Length);
+			outputBmp.UnlockBits(outputData);
+
+			return outputBmp;
+		}
+
+		public static int BlendingOperation(int pixel, int blend, BlendingMode mode) {
+			double p = (double)pixel / 255;
+			double b = (double)blend / 255;
+
+			double result = -1;
+
+			if (mode == BlendingMode.Multiply) {
+				result = p * b;
+			} else if (mode == BlendingMode.Overlay) {
+				result = (p > 0.5 ? 1 : 0) * (1 - (1 - 2 * (p - 0.5)) * (1 - b)) + (p <= 0.5 ? 1 : 0) * ((2 * p) * b);
+			} else if (mode == BlendingMode.Screen) {
+				result = 1 - (1 - p) * (1 - b);
+			} else if (mode == BlendingMode.LinearDodge) {
+				result = p + b;
+			}
+
+			return (int)(result * 255);
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e) {
+
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+				selectedFile = openFileDialog1.FileName;
+				label5.Text = "Selected file: " + selectedFile;
+			}
+		}
+
+		private void label3_Click(object sender, EventArgs e) {
+
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			CreateImage(selectedFile);
+		}
+
+		private void button3_Click(object sender, EventArgs e) {
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Filter = "PNG Image|*.png|JPG Image|*.jpg|BMP Image|*.bmp";
+			sfd.Title = "Save Generated Image";
+			sfd.ShowDialog();
+
+			if (sfd.FileName != "") {
+				FileStream fs = (FileStream)sfd.OpenFile();
+
+				switch (sfd.FilterIndex) {
+					case 1:
+						pictureBox1.Image.Save(fs, ImageFormat.Png);
+						break;
+					case 2:
+						pictureBox1.Image.Save(fs, ImageFormat.Jpeg);
+						break;
+					case 3:
+						pictureBox1.Image.Save(fs, ImageFormat.Bmp);
+						break;
+				}
+
+				fs.Close();
+			}
+		}
+	}
+}
